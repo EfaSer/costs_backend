6166067fadfacfa6632a01e6e5597feb9bc53dec
Ефанов Сергей Олегович <EfanovSO@lad.local>
2023-09-27 21:30:57 +0300

    update model

--- costs/api/models.py
+++ costs/api/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Cost(models.Model):
+  CHOICES = (
+    ("Supermarkets", "Supermarkets"),
+    ("Accounts", "Accounts"),
+    ("Other", "Other")
+  )
 
   description = models.TextField()
   amount = models.FloatField()
   date = models.TextField()
+  category = models.CharField(max_length=15, choices=CHOICES)
